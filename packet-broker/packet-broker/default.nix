629a6e555027471659dbd7529f517ecd854f7694
Alexander Gall <alexander.gall@gmail.com>
2020-10-19 15:11:10 +0200

    Add bf-sde to packetBroker
    This will inlcude the executables of the SDE in the
    packet-broker nix-env

--- packet-broker/packet-broker/default.nix
+++ packet-broker/packet-broker/default.nix
@@ -3,4 +3,7 @@
 {
   packetBroker = callPackage ./packet-broker.nix { inherit bf-sde; };
   configd = callPackage ./configd.nix { inherit bf-sde; };
+  ## Include a copy of the SDE. This is useful to include those
+  ## executables (in particular "bfshell") in a nix-env.
+  inherit bf-sde;
 }
