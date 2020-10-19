9674ed4a285b3f7f97a5b7aafd9d24097bbc4c31
Alexander Gall <alexander.gall@gmail.com>
2020-10-19 13:44:43 +0200

    Add Makefile, use kernelModule in buildP4Package
    All RARE packages can now be installed in the same environment.

--- RARE/RARE/default.nix
+++ RARE/RARE/default.nix
@@ -4,34 +4,39 @@ let
   fetchBitbucketPrivate = callPackage ./fetchbitbucket {
     identityFile = ./bitbucket_deployment_rsa;
   };
+  sal_modules = callPackage ./sal/modules.nix {
+    inherit fetchBitbucketPrivate;
+  };
+  kernelModule = "bf_kpkt";
 in rec {
   mpls = callPackage ./generic.nix {
-    inherit bf-sde;
+    inherit bf-sde kernelModule;
     flavor = "mpls";
     buildFlags = "-DHAVE_MPLS";
   };
   mpls_wedge100bf32x = callPackage ./generic.nix {
-    inherit bf-sde;
+    inherit bf-sde kernelModule;
     flavor = "mpls_wedge100bf32x";
     buildFlags = "-DHAVE_MPLS -D_WEDGE100BF32X_";
   };
   srv6 = callPackage ./generic.nix {
-    inherit bf-sde;
+    inherit bf-sde kernelModule;
     flavor = "srv6";
     buildFlags = "-DHAVE_SRV6";
   };
   srv6_wedge100bf32x = callPackage ./generic.nix {
-    inherit bf-sde;
+    inherit bf-sde kernelModule;
     flavor = "srv6_wedge100bf32x";
     buildFlags = "-DHAVE_SRV6 -D_WEDGE100BF32X_";
   };
   bf_forwarder = callPackage ./bf_forwarder.nix {
     inherit bf-sde sal_modules;
   };
-  sal_modules = callPackage ./sal/modules.nix {
-    inherit fetchBitbucketPrivate;
-  };
   sal_bf2556x = callPackage ./sal/bf2556x.nix {
     inherit fetchBitbucketPrivate sal_modules;
   };
+  ## Include a copy of the SDE. This is useful to include those
+  ## executables (in particular "bfshell") in an environment with
+  ## "nix-env -A RARE".
+  inherit bf-sde;
 }
--- RARE/RARE/generic.nix
+++ RARE/RARE/generic.nix
@@ -1,11 +1,14 @@
-{ bf-sde, fetchgit, flavor, buildFlags }:
+{ lib, bf-sde, fetchgit, flavor, buildFlags, kernelModule }:
 
 let
   repo = import ./repo.nix { inherit fetchgit; };
+  flavorStr = sep:
+    lib.optionalString (flavor != null) "${sep}${flavor}";
 in bf-sde.buildP4Program rec {
   inherit (repo) version src;
-  name = "RARE${if flavor == null then "" else "-${flavor}"}-${version}";
+  name = "RARE${flavorStr "-"}-${version}";
   p4Name = "bf_router";
   path = "p4src";
-  inherit buildFlags;
+  execName = "bf_router${flavorStr "_"}";
+  inherit buildFlags kernelModule;
 }
