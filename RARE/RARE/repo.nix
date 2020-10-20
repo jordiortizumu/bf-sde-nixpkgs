9c3329ec40143c13950b3b4ae791b11b02e09dad
Alexander Gall <alexander.gall@gmail.com>
2020-10-20 16:55:12 +0200

    Bump version of the RARE Git repo

--- RARE/RARE/repo.nix
+++ RARE/RARE/repo.nix
@@ -1,10 +1,10 @@
 { fetchgit }:
 
 {
-  version = "20.6.23";
+  version = "20.10.15";
   src = fetchgit {
     url = "https://bitbucket.software.geant.org/scm/rare/rare.git";
-    rev = "e2bece";
-    sha256 = "1ys8i0g97i9fnc573bc4xkr9vz4g0aqwfc57qa7cvp78vlvax1x3";
+    rev = "b3de54";
+    sha256 = "1q8il893qbdqpa4qp3p15aib0jl19qrkbchv9bnqlyx60lpb9b0q";
   };
 }
