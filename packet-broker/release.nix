73563f097540b61bcafb8b696b7d7d337be6c1ee
Alexander Gall <alexander.gall@gmail.com>
2021-01-22 11:32:21 +0100

    Add module wrapper to release.nix

--- packet-broker/release.nix
+++ packet-broker/release.nix
@@ -2,7 +2,12 @@
 
 with import ./. {};
 
-{
-  packetBroker = with lib; filterAttrs (n: v: attrsets.isDerivation v) packetBroker;
+let
+  kernels = import ../bf-sde/kernels pkgs;
+  broker = packetBroker.packetBroker;
+  wrappers = lib.mapAttrs (kernelID: _: broker.makeModuleWrapperForKernel kernelID) kernels;
+  packetBroker' = (with lib; filterAttrs (n: v: attrsets.isDerivation v) packetBroker);
+in {
+  packetBroker = packetBroker' // { inherit wrappers; };
   inherit SNMPAgent;
 }
