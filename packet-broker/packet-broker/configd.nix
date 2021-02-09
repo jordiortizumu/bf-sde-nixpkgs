eda4e565004e98f17ebb1014dddac5ab0ab8e44b
Alexander Gall <alexander.gall@gmail.com>
2021-02-09 14:12:41 +0100

    Use new sitePackagesPath attribute of bf-drivers

--- packet-broker/packet-broker/configd.nix
+++ packet-broker/packet-broker/configd.nix
@@ -1,28 +1,23 @@
-{ bf-sde, fetchFromGitHub, python2, makeWrapper }:
+{ bf-sde, fetchFromGitHub, makeWrapper }:
 
-python2.pkgs.buildPythonApplication rec {
+let
+  bf-drivers = bf-sde.pkgs.bf-drivers;
+  python = bf-drivers.pythonModule;
+in python.pkgs.buildPythonApplication rec {
   pname = "packet-broker-configd";
   version = "0.1";
 
   src = import ./repo.nix { inherit fetchFromGitHub; };
   propagatedBuildInputs = [
-    bf-sde.pkgs.bf-drivers
-    (python2.withPackages (ps: with ps; [ jsonschema ipaddress ]))
-  ];
+    bf-drivers
+  ] ++ (with python.pkgs; [ jsonschema ipaddress ]);
   buildInputs = [ makeWrapper ];
-
-  ## The Python environments (the one above and the one from the
-  ## propagated build inputs of bf-drivers) both contain the same
-  ## version of the "six" package, causing a conflict when they are
-  ## merged.
-  catchConflicts = false;
 
   preConfigure = ''cd control-plane'';
 
   postInstall = ''
     mkdir -p $out/etc/packet-broker
     cp config.json schema.json $out/etc/packet-broker
-    wrapProgram "$out/bin/configd.py" --set PYTHONPATH "${bf-sde.pkgs.bf-drivers}/lib/python2.7/site-packages/tofino"
+    wrapProgram $out/bin/configd.py --set PYTHONPATH ${bf-drivers.sitePackagesPath}/tofino
   '';
-
 }
